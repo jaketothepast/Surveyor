7ddb3276c4cc62f758190013027691791504e2d7
jaketothepast <jacob.windle@protonmail.com>
2017-12-30 13:58:36 -0500

    Removing Redis crap

--- redis_lib/utils.py
+++
@@ -1,7 +0,0 @@
-from redis import StrictRedis
-from django.conf import settings
-
-def get_client_connection():
-    return StrictRedis(host=settings.REDIS_HOST,
-                       port=settings.REDIS_PORT,
-                       db=settings.REDIS_DB)
