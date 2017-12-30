5070b0d58aa9a018d1765c0f2b3d79dd886190aa
jaketothepast <jacob.windle@protonmail.com>
2017-12-30 14:27:15 -0500

    Changing question view to do form handling

--- base/settings.py
+++ base/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'surveyor',
-    'redis_lib'
 ]
 
 MIDDLEWARE = [
--- surveyor/views.py
+++ surveyor/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
 from django.views.generic import CreateView
+from django.views.generic.edit import FormView
 from django.views.generic.detail import DetailView
 
 from surveyor.models import Question, Tag
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 def update_tags(request):
     """Update the tags for a question based on request"""
@@ -17,17 +18,15 @@ class NewQuestionView(CreateView):
     form_class = QuestionForm
     template_name = "question/new_question_form.html"
 
-class QuestionView(DetailView):
+class QuestionView(FormView):
     """Show either a newly created question, or expiration page"""
 
-    model = Question
     template_name = "question.html"
-
-    def post(self, request):
-        """Handle validation and saving of user response"""
-        pass
+    form_class = AnswerForm
+    success_url = '/question/new/'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        object = Question.objects.get(pk=kwargs['pk'])
         context['expired'] = self.object.expiration >= timezone.now()
         return context
