8cd01c2aa02228e6ed2c97f5756bf09ede4ab5b2
jaketothepast <windlejacob12@gmail.com>
2017-12-30 22:21:44 -0500

    Add functions to push to and remove from Redis

--- surveyor/utils.py
+++ surveyor/utils.py
@@ -10,3 +10,32 @@ def get_redis_connection():
     return redis.Redis(settings.REDIS_HOST,
                        settings.REDIS_PORT,
                        db=settings.REDIS_DB)
+
+# The thought here is that we store all data in a volatile manner
+# in Redis in memory. This allows for a certain degree of anonyminity,
+# 
+# Question ID's are pushed and popped from a set keyed on a user email
+# (which will go away after we deliver the report, possibly using expiring keys)
+#
+# Answers will be pushed under question ID's somehow, and popped on report
+# Creation time.
+
+def question_to_redis(email, question_id):
+    """Push to a list under a certain email on Redis"""
+    
+    conn = get_redis_connection()
+
+    conn.sadd(email, question_id)
+
+def question_from_redis(email, question_id):
+    """Remove a question id from the set in Redis"""
+
+    conn = get_redis_connection()
+
+    result = conn.spop(email, question_id)
+
+    # spop returns a nil if nothing was popped
+    if not result:
+        return False
+
+    return True
