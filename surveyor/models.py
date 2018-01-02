b275c2b696b047ac40cef2a1695e7aa4dfb703c1
Daniel Hones <danielhones@gmail.com>
2018-01-02 02:52:35 -0500

    add QuestionTag model, remove reference to tag from Question model

--- surveyor/models.py
+++ surveyor/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 
 
 class Tag(models.Model):
-    question = models.ForeignKey("Question", on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
 
@@ -18,3 +17,8 @@ class Question(models.Model):
 class Answer(models.Model):
     question = models.ForeignKey("Question", on_delete=models.CASCADE)
     text = models.CharField(max_length=500)
+
+
+class QuestionTag(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
