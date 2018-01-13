e22fc2bbe45f3dc0557f127e41197a78c2e5f654
Jacob Windle <windlejacob12@gmail.com>
2018-01-13 14:35:52 -0500

    Made webpack file version 2 compliant

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,38 @@ module.exports = {
         // Where to store bundle metadata
         new BundleTracker({filename: './webpack-stats.json'}),
         // Make JQuery available in modules
+        // Point the node module at variables it exposes
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
             'window.jQuery': 'jquery'
         })
     ],
+
+    module: {
+        loaders: [
+            //Regex that tells webpack to use loaders
+
+            // Load JS and JSX files
+            {test: /\.jsx?$/,
+
+             // Dont transpile all files in node_modules
+             exclude: /node_modules/,
+
+             // Use the Babel loader
+             loader: 'babel-loader',
+
+             query: {
+                 // We will be dealing with React
+                 presets: ['react']
+             }}
+        ]
+    },
+
+    resolve: {
+        //Where to look for modules
+        modules: ['node_modules'],
+        // Extensions used to resolve modules
+        extensions: ['.js', '.jsx']
+    }
 }
