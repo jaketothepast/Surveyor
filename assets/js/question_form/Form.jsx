ab82522bb91bf08909bdce742d4741f45210a94c
Jacob Windle <windlejacob12@gmail.com>
2018-01-13 17:59:21 -0500

    Adding in magic lifecycle hooks for the Form class

--- assets/js/question_form/Form.jsx
+++ assets/js/question_form/Form.jsx
@@ -5,9 +5,38 @@ import ReactDOM from 'react-dom';
  * Factory for creating form components for questions
  */
 class Form extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {formComponents: new FormComponents()};
+    }
+
+    /* Lifecycle hooks */
+
+    /* Runs after component output rendered to DOM */
+    componentDidMount() {
+
+    }
+
+    /* Runs after removed from DOM */
+    componentWillUnmount() {
+
+    }
+
+    /* Need a call to this.setState for button pushes */
+
+    newFormComponent() {
+        this.state.formComponents.push(new FormComponent());
+    }
+
     render() {
-        return <h1>FormBuilder</h1>;
+        return (
+            <h1>FormBuilder</h1>
+            <input type="button">Create Form Component</input>
+            <FormComponents />
+        );
     }
 }
 
 ReactDOM.render(<Form />, document.getElementById('mount'))
+https://angel.co/
