db3c7031db36a406d676da8848e7506245f987e2
jaketothepast <windlejacob12@gmail.com>
2018-01-12 19:19:52 -0800

    Trying really hard to get back into this

--- surveyor/static/js/FormComponent.js
+++ surveyor/static/js/FormComponent.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 /**
  * Factory for creating form components for questions
  */
-class FormComponent {
-    
+class FormBuilder extends React.Component {
+    render() {
+        return <h1>FormBuilder</h1>;
+    }
 }
+
+ReactDOM.render(<FormBuilder />, document.getElementById("mount"));
